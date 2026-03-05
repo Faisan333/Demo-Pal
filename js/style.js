f4799a99e93acc9d412a2a18ea31d75e7bcb5629
MUHAMMED FAISAN <azeezfaizan@gmail.com>
2026-03-06 00:46:18 +0400

    changes

--- js/style.js
+++ js/style.js
@@ -56,3 +56,19 @@ var swiper = new Swiper(".trending-slider", {
     }
 
 });
+
+
+
+const button = document.getElementById("wa_button");
+const popup = document.getElementById("wa_popup");
+
+button.addEventListener("click", function(){
+
+if(popup.style.display === "block"){
+popup.style.display = "none";
+}
+else{
+popup.style.display = "block";
+}
+
+});
